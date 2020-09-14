5af84a8e96eca56321cb72df0f97917003d8b7a6
SAHITHYA1998 <sahithyahi@gmail.com>
2020-09-14 22:39:04 +0530

    Added:message1

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,2 +1,7 @@
-echo "Welcome to arithmetic computation
-"
+#! /bin/bash -x
+echo "Welcome to Arithmetic Computation Programming"
+
+read -p "Enter first input " a
+read -p "Enter second input " b
+read -p "Enter third input " c
+
