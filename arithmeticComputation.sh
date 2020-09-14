2ae733bd6c793b7eb8f2d6b9d8290157afdd997e
SAHITHYA1998 <sahithyahi@gmail.com>
2020-09-14 22:54:02 +0530

    Added:message5

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -5,7 +5,5 @@ read -p "Enter first input " a
 read -p "Enter second input " b
 read -p "Enter third input " c
 
-
-Comput3=`awk 'BEGIN{printf("%.2f",'$a' + '$b' / '$c')}'`
-echo  $Comput3
-
+Comput4=`awk 'BEGIN{printf("%.2f",'$a' % '$b' + '$c')}'`
+echo  $Comput4
