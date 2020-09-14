c11c38065a46027b7674bc5042b26d37a09fc49b
SAHITHYA1998 <sahithyahi@gmail.com>
2020-09-15 00:15:29 +0530

    Added:message8

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,4 +1,4 @@
-#! /bin/bash -x
+#! /bin/bash 
 declare -A result
 echo "Welcome to Arithmetic Computation Programming"
 
@@ -22,3 +22,31 @@ result[3]="$Comput3"
 result[4]="$Comput4"
 
 echo "Dictionary :  ${result[@]}"
+
+echo "Array : "
+for i in ${!result[@]}
+do
+        arr[i]=${result[$i]}
+        echo -n "${arr[i]} "
+done
+printf "\n"
+echo "Results in Descending Order "
+
+n=${#result[@]}
+
+for (( i=1;i<=n-1;i++ ))
+do
+        for (( j=i;j<=n-i;j++ ))
+        do
+        if [[ ${arr[$((j+1))]%%.*} -gt ${arr[j]%%.*} ]]
+        then
+                temp=${arr[j+1]}
+                arr[$((j+1))]=${arr[j]}
+                arr[j]=$temp
+        fi
+        done
+done
+for ((i=1;i<=n;i++))
+do
+        echo -n "${arr[$i]} "
+done
